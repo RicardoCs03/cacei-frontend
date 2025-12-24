c15fe1f496cfb70c1b01b6d9f402b30b6d3151ff
RicardoCs03 <Ricardo.ContrerasS@outlook.com>
2025-12-23 19:02:19 -0600

    Carpetas que contienen logica de negocio

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { LoginRequest, LoginResponse } from '../models/auth.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:9090/api/auth';//URL base de la API de autenticación
+
+  //Constructor de la clase
+  constructor(private http: HttpClient) {}
+
+  login(data: LoginRequest): Observable<LoginResponse> {// Método para iniciar sesión
+    return this.http.post<LoginResponse>(`${this.apiUrl}/login`, data);
+  }
+}
