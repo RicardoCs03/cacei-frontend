629a49ed117a04f4b68128d0190f9e4506fbc423
RicardoCs03 <Ricardo.ContrerasS@outlook.com>
2025-12-26 18:18:29 -0600

    Modificacion de las rutas para mostrar primero el login y segun el logueo muestre el panel del admin o del profesor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,31 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { authGuard } from './core/guards/auth-guard';
+import { roleGuard } from './core/guards/role-guard';
 export const routes: Routes = [
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
-    { path: 'login', component: LoginComponent }
+    { path: 'login', component: LoginComponent },
+    { path: '', 
+        component:MainLayoutComponent,
+        canActivate: [authGuard],
+        children: [
+            {
+                path: 'admin',
+                canActivate: [roleGuard('ROLE_ADMINISTRADOR')],
+                loadComponent: () => 
+                    import('./layout/admin/admin.component')
+                .then(m => m.AdminComponent)
+    },
+    {
+        path: 'profesor',
+        canActivate: [roleGuard('ROLE_PROFESOR')],
+        loadComponent: () => 
+            import('./layout/profesor/profesor.component')
+        .then(m => m.ProfesorComponent)
+    }
+        ]
+    },  
+
+    {path: '**', redirectTo: 'login'}
+
 ];
