82415c10581f41a59b8703ea693a031829e47508
RicardoCs03 <Ricardo.ContrerasS@outlook.com>
2025-12-28 14:09:40 -0600

    irrelevante

--- src/app/layout/main-layout/main-layout.component.ts
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -2,16 +2,19 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TokenService } from '../../core/services/token.service';
 import { Router,RouterOutlet } from '@angular/router';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-main-layout.component',
-  imports: [CommonModule, RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, SidebarComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css',
 })
 export class MainLayoutComponent {
 
   role: string | null = '';
+  
 
   constructor(private tokenService: TokenService,private router: Router){
     this.role = this.tokenService.getRole();
