388b99b2ef9460336b7814c4fc85e31c4b8d9858
RicardoCs03 <Ricardo.ContrerasS@outlook.com>
2025-12-31 15:59:53 -0600

    Implementacion del modelo de programa educativo

---
+++ src/app/core/models/programaEducativo.model.ts
@@ -0,0 +1,18 @@
+export interface ProgramaEducativo {
+    id?: number;
+    nombre: string;
+    totalCreditos: number;
+    tPermanenciaMin: number;
+    tPermanenciaMax: number;
+    fechaCreacion: string;
+    contraccion: string;
+    isActive: boolean;
+    nivelEstudio: string; //TODO: MODIFICAR TIPO A INTERFACE NivelEstudio
+    modalidad: string; // TODO: MODIFICAR TIPO A INTERFACE Modalidad
+    //atributosEgreso?: AtributoEgreso[];
+    //atributosCacei?: AtributoCacei[];
+    //objetivosEspecificos?: ObjetivoEspecifico[];
+    //experienciasEducativas?: ExperienciaEducativa[];
+    //alumnos?: Alumno[];
+    //coordinadorCacei?: CoordinadorCacei;
+}  
