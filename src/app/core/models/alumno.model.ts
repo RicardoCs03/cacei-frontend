4ffcd1720e4d311d03673bec2c08bde2e5754d12
RicardoCs03 <Ricardo.ContrerasS@outlook.com>
2026-01-01 14:07:30 -0600

    Implementacion del modelo DTO de alumnos

---
+++ src/app/core/models/alumno.model.ts
@@ -0,0 +1,9 @@
+export interface Alumno{
+    id?: number;
+    nombre: string;
+    apepat: string;
+    apemat: string;
+    matricula: string;
+    programaEducativo: string;
+    isActive?: boolean;
+}
