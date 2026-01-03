6754fd16c07b35095257f847d29169c6c025b589
RicardoCs03 <Ricardo.ContrerasS@outlook.com>
2026-01-03 13:13:39 -0600

    Bug: No contaba con router-outlet

--- src/app/layout/profesor/profesor.component.ts
+++ src/app/layout/profesor/profesor.component.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-profesor.component',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './profesor.component.html',
   styleUrl: './profesor.component.css',
 })
